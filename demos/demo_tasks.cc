c46e48fa9536ad74fe54f7c4339cf298d4d532b0
erdieee <58039191+erdieee@users.noreply.github.com>
2023-10-25 21:35:54 +0200

    feat: taskmangaer now has a method to return the number of threads

--- demos/demo_tasks.cc
+++ demos/demo_tasks.cc
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <sstream>
 
-
 #include <taskmanager.h>
 #include <timer.h>
 
 using namespace ASC_HPC;
 using std::cout, std::endl;
-
 
 int main()
 {
@@ -15,52 +13,39 @@ int main()
 
   TaskManager t;
   t.StartWorkers();
-  
-  TaskManager::RunParallel([] (int i, int size)
-  {
+  cout << "Running with " << t.getNumThreads() << " threads" << endl;
+
+  TaskManager::RunParallel([](int i, int size)
+                           {
     static Timer t("timer one");
     RegionTimer reg(t);
-      cout << "I am task " << i << " out of " << size << endl;
-  });
+      cout << "I am task " << i << " out of " << size << endl; });
 
-  
-  TaskManager::RunParallel([] (int i, int s)
-  {
-    TaskManager::RunParallel([i] (int j, int s2)
-    {
+  TaskManager::RunParallel([](int i, int s)
+                           { TaskManager::RunParallel([i](int j, int s2)
+                                                      {
       std::stringstream str;
       str << "nested, i,j = " << i << "," << j << "\n";
-      cout << str.str();
-    });
-  });
+      cout << str.str(); }); });
 
-
-
-  
-  TaskManager::RunParallel([] (int i, int size)
-  {
+  TaskManager::RunParallel([](int i, int size)
+                           {
     static Timer t("timer two", { 0, 0, 1});
-    RegionTimer reg(t);
-  });
+    RegionTimer reg(t); });
 
-  TaskManager::RunParallel([] (int i, int size)
-  {
+  TaskManager::RunParallel([](int i, int size)
+                           {
     static Timer t("timer 3", { 1, 0, 0});
-    RegionTimer reg(t);
-  });
+    RegionTimer reg(t); });
 
-  TaskManager::RunParallel([] (int i, int s)
-  {
+  TaskManager::RunParallel([](int i, int s)
+                           {
     static Timer t("timer 4", { 1, 1, 0});
     RegionTimer reg(t);    
     TaskManager::RunParallel([i](size_t j, size_t s2)
     {
       ;
-    });
-  });
+    }); });
 
-
-  
   t.StopWorkers();
 }
-
